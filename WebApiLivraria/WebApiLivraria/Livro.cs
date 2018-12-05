f21d2a48a71c619986790ca8e670ee9dbddfcee1
Admin <admin@Admins-MacBook.local>
2018-12-05 00:22:23 -0200

    Adicionando projeto WebApiLivraria

---
+++ WebApiLivraria/WebApiLivraria/Livro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace WebApiLivraria
+{
+
+    /*
+     *  ISBN, Autor, Nome, Preço, Data Publicação, Imagem da Capa
+     */
+    public class Livro
+    {
+        public Livro()
+        {
+        }
+
+        public String ISBN { get; set; }
+        public String Autor { get; set; }
+        public String Nome { get; set; }
+        public Double Preco { get; set; }
+        public DateTime DataPublicacao { get; set; }
+        public byte[] Imagem { get; set;  }
+
+    }
+}
