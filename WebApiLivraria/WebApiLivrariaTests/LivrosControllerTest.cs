57fd12a7dc1d98c3404e4173bc9770e80fba131f
newton.carvalho <newton.carvalho@dataprev.gov.br>
2018-12-06 17:05:41 -0200

    Criando projeto para testar Controllers e DBContext do projeto WebApiLivraria

---
+++ WebApiLivraria/WebApiLivrariaTests/LivrosControllerTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApiLivraria.Controllers;
+
+namespace WebApiLivrariaTests
+{
+    [TestClass]
+    public class LivrosControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
